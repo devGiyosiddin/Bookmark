ba0f1354a8795f6cd346b6148241e560751ff8fc
devGiyosiddin <devgiyosiddin@gmail.com>
2023-11-25 01:00:35 +0500

    Add subscription section

--- JS/main.js
+++ JS/main.js
@@ -2,16 +2,26 @@ const elsTabsItem = document.querySelectorAll('.tabs__item');
 const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
 
+const elsAccordionItem = document.querySelectorAll('.accordion__item');
+const elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
+
 function deactivateTabItems () {
     elsTabsItem.forEach(function (elTabsItem) {
         elTabsItem.classList.remove('tabs__item--active');
     });
 }
+
 function deactivatetabPanels () {
     elsTabsPanel.forEach(function (elTabsPanel) {
         elTabsPanel.classList.remove('tabpanels__item--active');
     });
 }
+
+function closeAccordionItems (){
+    elsAccordionItem.forEach(function (elAccordionItem) {
+        elAccordionItem.classList.remove('accordion__item--open');
+    });
+};
 
 elsTabLink.forEach(function (elTabLink) {
     elTabLink.addEventListener('click', function(evt) {
@@ -33,3 +43,11 @@ elsTabLink.forEach(function (elTabLink) {
         elTargetPanel.classList.add('tabpanels__item--active');
     });
 });
+
+elsAccordionItemToggler.forEach(function(elAccordionItemToggler) {
+    elAccordionItemToggler.addEventListener('click', function() {
+        closeAccordionItems();
+
+        elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
+    })
+})
