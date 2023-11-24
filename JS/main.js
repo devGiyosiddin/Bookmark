6a15401e34813a8210c5cd259d8761eb11a0b7ec
devGiyosiddin <devgiyosiddin@gmail.com>
2023-11-24 14:34:31 +0500

    BEM

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 const elsTabsItem = document.querySelectorAll('.tabs__item');
-const elsTabsPanel = document.querySelectorAll('.tabs__panel');
+const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
 
 function deactivateTabItems () {
@@ -9,7 +9,7 @@ function deactivateTabItems () {
 }
 function deactivatetabPanels () {
     elsTabsPanel.forEach(function (elTabsPanel) {
-        elTabsPanel.classList.remove('tabs__panel--active');
+        elTabsPanel.classList.remove('tabpanels__item--active');
     });
 }
 
@@ -30,6 +30,6 @@ elsTabLink.forEach(function (elTabLink) {
         // Show active tab panel
         // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
         const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
-        elTargetPanel.classList.add('tabs__panel--active');
+        elTargetPanel.classList.add('tabpanels__item--active');
     });
 });
